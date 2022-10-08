0c23eff89d7eeb0090d97f23ef483cb70aea1dc4
Ray Antonius <r.antonius@verihubs.com>
2022-10-09 06:42:42 +0700

    feat: add error handling

--- internal/delivery/rest/user_handler.go
+++ internal/delivery/rest/user_handler.go
@@ -17,7 +17,7 @@ func (h *handler) RegisterUser(c echo.Context) error {
 			"err": err,
 		}).Error("[delivery][rest][user_handler][RegisterUser] unable to decode request")
 
-		return c.JSON(http.StatusOK, map[string]interface{}{
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
 			"error": err.Error(),
 		})
 	}
@@ -28,7 +28,7 @@ func (h *handler) RegisterUser(c echo.Context) error {
 			"err": err,
 		}).Error("[delivery][rest][user_handler][RegisterUser] unable to register user")
 
-		return c.JSON(http.StatusOK, map[string]interface{}{
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
 			"error": err.Error(),
 		})
 	}

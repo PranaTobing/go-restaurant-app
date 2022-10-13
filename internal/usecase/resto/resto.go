fa14dd37fb7e4a09616cf0ec7c08df358b2d7674
Ray Antonius <r.antonius@verihubs.com>
2022-10-13 19:43:22 +0700

    fix

--- internal/usecase/resto/resto.go
+++ internal/usecase/resto/resto.go
@@ -42,7 +42,7 @@ func (m *restoUsecase) Order(request model.OrderMenuRequest) (model.Order, error
 			ID:         uuid.New().String(),
 			OrderCode:  orderProduct.OrderCode,
 			Quantity:   orderProduct.Quantity,
-			TotalPrice: menuData.Price & int64(orderProduct.Quantity),
+			TotalPrice: menuData.Price * int64(orderProduct.Quantity),
 			Status:     constant.ProductOrderStatusPreparing,
 		}
 	}
